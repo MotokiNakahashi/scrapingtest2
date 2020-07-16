df1b2e72591fdd10af2cce3f62611c8648fd15e3
MotokiNakahashi <66356155+MotokiNakahashi@users.noreply.github.com>
2020-07-16 16:56:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/scrapingtest2/AsyncHttpRequest.java
@@ -0,0 +1,68 @@
+package com.example.scrapingtest2;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.widget.TextView;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public final class AsyncHttpRequest extends AsyncTask<URL, Void, String> {
+    //フィールドと変数の設定
+    private Activity mainActivity;
+    String url1 = "https://news.yahoo.co.jp/categories/domestic";
+
+
+    //コンストラクタの設定
+    public AsyncHttpRequest(Activity activity) {
+        this.mainActivity = activity;
+    }
+
+
+    @Override
+    protected String doInBackground(URL... urls) {
+        final URL url = urls[0];
+        HttpURLConnection con = null;
+
+        try {
+            //httpコネクトの設定
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setInstanceFollowRedirects(false);
+            con.connect();
+            final int statusCode = con.getResponseCode();
+            if (statusCode != HttpURLConnection.HTTP_OK) {
+                System.err.println("正常に接続できていません。statusCode:" + statusCode);
+                return null;
+            }
+
+            // Jsoupで対象URLの情報を取得する。
+            Document doc = Jsoup.connect(url1).get();
+            Elements elm = doc.select("li.topicsListItem");
+            Element elm2 = elm.get(6);
+            String title = elm2.text();
+            return  title;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+
+        } finally {
+            if (con != null) {
+                con.disconnect();
+            }
+        }
+    }
+
+
+    //ボタンを押した時の動作
+    @Override
+    protected void onPostExecute(String result) {
+        TextView tv = mainActivity.findViewById(R.id.messageTextView);
+        tv.setText(result);
+    }
+}
